3c953920c2e17c7f060588a0e299301ab876c619
Peter <kuanjie.chen@mail.utoronto.ca>
2025-06-19 11:45:23 +0800

    misc

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -2,42 +2,42 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // set this to your Django backend API
-  timeout: 5000,
+	baseURL: 'http://localhost:8000', // set this to your Django backend API
+	timeout: 5000,
 });
 
 axiosInstance.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  }
+	config => {
+		const token = localStorage.getItem('access_token');
+		if (token) {
+			config.headers['Authorization'] = `Bearer ${token}`;
+		}
+		return config;
+	}
 );
 
 axiosInstance.interceptors.response.use(
-  response => response,
-  async error => {
-    const originalRequest = error.config;
+	response => response,
+	async error => {
+		const originalRequest = error.config;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      const refreshToken = localStorage.getItem('refreshToken');
+		if (error.response?.status === 401 && !originalRequest._retry) {
+			originalRequest._retry = true;
+			const refreshToken = localStorage.getItem('refreshToken');
 
-      try {
-        const res = await axios.post('/api/accounts/refresh/', { refresh: refreshToken });
-        const newAccessToken = res.data.access;
-        localStorage.setItem('accessToken', newAccessToken);
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-        return axiosInstance(originalRequest);
-      } catch (err) {
-        window.location.href = '/';
-      }
-    }
+			try {
+				const res = await axios.post('/api/accounts/refresh/', { refresh: refreshToken });
+				const new_access_token = res.data.access;
+				localStorage.setItem('access_token', new_access_token);
+				originalRequest.headers['Authorization'] = `Bearer ${new_access_token}`;
+				return axiosInstance(originalRequest);
+			} catch (err) {
+				window.location.href = '/';
+			}
+		}
 
-    return Promise.reject(error);
-  }
+		return Promise.reject(error);
+	}
 );
 
 export default axiosInstance;
